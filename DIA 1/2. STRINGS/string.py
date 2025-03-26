2e416454093a785de6299d926abbb4fe825993d7
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-25 19:35:40 -0500

    Strings

---
+++ DIA 1/2. STRINGS/string.py
@@ -0,0 +1,8 @@
+print("100+5")
+print("Hola 'Murphy'")
+print("Hola" + " "+ 'Murphy')
+print("Me llamo \"Murphy\"")
+print("Esta es una linea \ny esta es otra linea")
+print("\t Esta es la tercera linea")
+print('This isn\'t a number')
+print("Este signo \\ es una barra invertida")
