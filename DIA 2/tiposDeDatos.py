9127287cafa0b92acd95b07d8b887df3d07ea573
🩺MURPHY SEQUEIROS💻 <168346448+DrMurphyUS@users.noreply.github.com>
2025-03-26 17:38:39 -0500

    Tipos de Datos

---
+++ DIA 2/tiposDeDatos.py
@@ -0,0 +1,46 @@
+# Tipos de datos en Python con ejemplos
+
+# String (str)
+cadena_1 = "hola"
+cadena_2 = "%$&*"
+cadena_3 = " "
+cadena_4 = "123"
+
+# Integer (int)
+entero_1 = 150
+entero_2 = 1
+entero_3 = 555
+entero_4 = -15
+entero_5 = 0
+
+# Floating point (float)
+flotante_1 = 1.25
+flotante_2 = 25.0
+flotante_3 = 500.50
+flotante_4 = -95.1
+
+# Listas (list)
+lista = ["sal", 1, -3, 4.5, "marte", 0]
+
+# Diccionarios (dict)
+diccionario = {'color': 'rojo', 'arte': 'cine'}
+
+# Tuplas (tuple)
+tupla = ('lun', 'mar', 'mie', 'jue', 'vie')
+
+# Sets (set)
+conjunto = {'a', 'b', 'c', 'd', 'e'}
+
+# Booleanos (bool)
+booleano_1 = True
+booleano_2 = False
+
+# Imprimir todos los valores
+print("Strings:", cadena_1, cadena_2, cadena_3, cadena_4)
+print("Enteros:", entero_1, entero_2, entero_3, entero_4, entero_5)
+print("Flotantes:", flotante_1, flotante_2, flotante_3, flotante_4)
+print("Lista:", lista)
+print("Diccionario:", diccionario)
+print("Tupla:", tupla)
+print("Set:", conjunto)
+print("Booleanos:", booleano_1, booleano_2)
