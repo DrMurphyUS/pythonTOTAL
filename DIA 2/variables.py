d729c9034b00f174a6c036b3568400519f850c5b
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-27 09:14:32 -0500

    Variables

---
+++ DIA 2/variables.py
@@ -0,0 +1,13 @@
+nombre = "Juan"
+print(nombre)
+nombre = "Laura"
+print(nombre)
+edad = 30
+edad2 = 15
+print(edad + edad2)
+apodo = input("Dime tu apodo: ")
+print("Tu apodo es: " + apodo)
+hijodelpadre = "Romulo"
+apellidodelpadre = "Sequeiros"
+nombrecompletodelhijo = hijodelpadre + " " + apellidodelpadre
+print(nombrecompletodelhijo)
