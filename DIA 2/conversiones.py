585811de2383e43dfff57e8a28d0d88eee8b880d
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-27 18:22:25 -0500

    conversiones

---
+++ DIA 2/conversiones.py
@@ -0,0 +1,24 @@
+#la convesion se llama casting
+#implicita q es automatico y explicita cuando yo digo como la voy a transformar enque tipo de dato
+num1 = 20
+num2 = 30.5
+print(type(num1))
+print(type(num2))
+num3 = num1 + num2
+print(type(num3))
+#esto seria una conversion explicita
+
+num4 = 5.8
+print(num4)
+print(type(num4))
+num5 = int(num4)
+print(num5)
+print(type(num5))
+
+edad = input("Dime tu edad: ")
+print(edad)
+print(type(edad))
+edad = int(edad)
+print(edad)
+print(type(edad))
+
