c573d791a3f482ea50bb950a078ed3d5764e19ed
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-27 18:56:32 -0500

    operadores matematicos

---
+++ DIA 2/operadores.py
@@ -0,0 +1,16 @@
+x=6
+y=2
+z=7
+print(f"{x} mas {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} entre {y} es igual a {x/y}")
+#division al piso
+print(f"{z} divido al piso con {y} es igual a {z//y}")
+#modulo lo que sobraria de una division
+print(f"{z} modulo con {y} es igual a {z%y}")
+#elevado
+print(f"{x} elevado al {y} es igual a {x**y}")
+print(f"{x} elevado al {3} es igual a {x**3}")
+#raiz cuadrada
+print(f"la raiz cuadrada de {x} es {x**0.5}")
