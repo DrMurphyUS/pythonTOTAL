eeccca76758a5bca796c796280ea2fffb5378fcc
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-27 17:40:01 -0500

    buenas practicas en los nombres de variables

---
+++ DIA 2/nombreVariables.py
@@ -0,0 +1,13 @@
+#legible
+nombre_perro = "Simba"
+#unidad
+nombre_de_estudiante = "Juan"
+#hispanismos no van
+arbol
+musica
+Genero
+anios
+#numero no al comienzo
+mes_8 = "agosto"
+#no signos :<>/()
+#no puedes usar palabras clave input print string int
