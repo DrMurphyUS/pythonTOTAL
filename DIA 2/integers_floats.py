76bb800f3fd257d9f6bcbafcc82da69e7aa65143
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-27 17:52:43 -0500

    Integers y floats

---
+++ DIA 2/integers_floats.py
@@ -0,0 +1,9 @@
+mi_numero = 5.8
+print(mi_numero + mi_numero)
+print(type(mi_numero))
+edad = input("Dime tu edad: ")
+print("Tu edad es: " + edad)
+#nueva_edad = 1 + edad
+#print("vas a cumplir " + nueva_edad)
+#input es tomado como str
+print(type(edad))
