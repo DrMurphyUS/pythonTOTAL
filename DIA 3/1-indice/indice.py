a0f4e3080ebe255afbfdfdd2f87d89134ec4452e
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-28 13:49:39 -0500

    Index y Rindex

---
+++ DIA 3/1-indice/indice.py
@@ -0,0 +1,12 @@
+mi_texto = "Esta es una prueba"
+resultado = mi_texto[-2]
+print(resultado)
+resultado2 = mi_texto.index("a")
+print(resultado2)
+resultado3 = mi_texto.index("a",5,11)
+print(resultado3)
+resultado4 = mi_texto.index("a",11)
+print(resultado4)
+#rindex busca de derecha a izquierda al revez
+resultado5 = mi_texto.rindex("a")
+print(resultado5)
