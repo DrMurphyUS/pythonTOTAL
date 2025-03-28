736d3ccc9848dcca57204672e34f25470772014d
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-27 19:10:23 -0500

    redondeo

---
+++ DIA 2/redondeo.py
@@ -0,0 +1,15 @@
+print(90/7)
+#redondeo hacia arriba
+print(round(90/7))
+resultado = round(90/7)
+
+print(resultado)
+valor =  round(95.66666666)
+print(valor)
+print(type(valor))
+valor =  round(95.66666666,2)
+print(valor)
+print(type(valor))
+valor2 = 95.666666
+print(round(valor2))
+print(type(valor2))
