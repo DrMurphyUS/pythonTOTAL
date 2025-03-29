8717f9de86c42b96c26a4e586ceca785f0954c1b
DrMurphyUS <medicosequeiros@gmail.com>
2025-03-29 09:22:02 -0500

    Propiedades Strings

---
+++ DIA 3/4- propiedadestrings/propiedadesstrings.py
@@ -0,0 +1,9 @@
+#no se puede cambiar  [0] strings letras se llaman inmutables
+n1 = "Kar"
+n2 = "ina"
+print(n1+n2)
+poema = """Mil pequeños peses blancos
+como si hiriviera el color de agua"""
+print(poema)
+print("agua" in poema)
+print(len(poema))
