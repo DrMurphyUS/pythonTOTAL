203261598baeac08c3b35dc47f46f42fdfca142c
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-01 13:02:23 -0500

    Booleanos

---
+++ DIA 3/9- Booleanos/booleano.py
@@ -0,0 +1,21 @@
+var1 = True
+var2 = False
+print(type(var1))
+print(var1)
+print(var2)
+
+numero = 5 > 2+3
+print(numero)
+print(type(numero))
+
+numero2= 5 == 3+2
+print(numero2)
+numero3= 5 !=3+2
+print(numero3)
+
+numero4 = bool(6==3)
+print(numero4)
+
+lista = [1,2,3,4]
+control = 6 not in lista
+print(control)
