666aac78df6bad19fd3337dc34d2784a3ca28911
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-03 09:49:45 -0500

    Operadores Logicos

---
+++ DIA 4/2- Operadores Logicos/operadoresLogicos.py
@@ -0,0 +1,14 @@
+
+mi_bool= (4<5) and (5 == 2+3)
+print(mi_bool)
+mi_bool2= (55==55) and ('PERRO'=='PERRO')
+print(mi_bool2)
+mi_bool3 = 1==10 or 2==2
+print(mi_bool3)
+texto = 'este texto es breve'
+mi_bool4 = ('frase' in texto) or ('breve' in texto)
+print(mi_bool4)
+mi_bool5 = not ('a' != 'a')
+print(mi_bool5)
+mi_bool6 = not True
+print(mi_bool6)
