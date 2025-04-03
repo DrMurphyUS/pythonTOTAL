7fe6e2fea7dbcd2ff30e55d59f75122a2f7634a2
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-03 17:47:14 -0500

    Control de Flujo

---
+++ DIA 4/3- Control de Flujo/controlFlujo.py
@@ -0,0 +1,21 @@
+mascota = 'pez'
+if mascota == 'gato':
+    print('Tienes un gato')
+elif mascota =='perro':
+    print('Tienes un perro')
+elif mascota =='pez':
+    print('Tienes un pez')
+else:
+    print('No se que animal tienes')
+
+edad =16
+calificacion = 12
+if edad < 18:
+    print('Eres menor de edad')
+    if calificacion >= 11:
+        print('Aprobado eres un cojonudo')
+    else:
+        print('Puta que burro jalaste el examen')
+else:
+    print('Eres adulto')
+
