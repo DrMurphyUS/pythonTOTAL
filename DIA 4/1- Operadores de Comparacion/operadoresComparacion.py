e399f0444e42be42f070d3bd0c5390f1e3dede51
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-03 09:17:45 -0500

    Operadores de Comparación

---
+++ DIA 4/1- Operadores de Comparacion/operadoresComparacion.py
@@ -0,0 +1,14 @@
+mi_variable = 'hola mundo'
+mi_variable2 = 6+6==20-8
+mi_variable3 = 'blanco'=='Blanco'.lower()
+mi_variable4 = '200'==200
+mi_variable5 = 100.00 == 100
+mi_variable6 = 100!=99
+mi_variable7 = 5>=6
+print(mi_variable)
+print(mi_variable2)
+print(mi_variable3)
+print(mi_variable4)
+print(mi_variable5)
+print(mi_variable6)
+print(mi_variable7)
