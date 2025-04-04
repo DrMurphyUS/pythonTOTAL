76a573d52fb2ff844de526a03ab12856107b3242
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-03 19:22:37 -0500

    Rango

---
+++ DIA 4/6- Rango/rango.py
@@ -0,0 +1,11 @@
+lista = [1,2,3,4]
+for  numero in lista:
+    print(numero)
+
+for numeros in range(5):
+    print(numeros)
+for numerosa in range(10,25,2):
+    print(numerosa)
+
+listas = list(range(1,101))
+print(listas)
