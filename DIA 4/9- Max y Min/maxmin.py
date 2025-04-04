91c50d114c4c72b8eeb11c02b1e011ec3d5790b3
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-04 10:49:48 -0500

    Max Min

---
+++ DIA 4/9- Max y Min/maxmin.py
@@ -0,0 +1,21 @@
+menor = min(9,12,23,323,11)
+mayor = max(9,12,23,323,11)
+print(menor)
+print(mayor)
+lista = [12,549,4548,121,7,3,4587]
+
+print(min(lista))
+print(max(lista))
+print(f'el menor es {min(lista)} y el mayor es {max(lista)}')
+
+nombres = ['juan','jose','rafaella','andrea']
+print(max(nombres))
+print(min(nombres))
+
+dic = {'c1':21,'c2':1,'c3':3}
+print(max(dic))
+print(min(dic))
+print(max(dic.values()))
+print(min(dic.values()))
+
+
