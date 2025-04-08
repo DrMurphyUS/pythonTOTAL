83d742bc93b3cbb9efce4c099836d293da499589
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-08 12:39:53 -0500

    Return

---
+++ DIA 5/3- Return/returnion.py
@@ -0,0 +1,10 @@
+#con return podemos almacenar lo que la funcion devuelva en una variable
+def multiplicar(numero1,numero2):
+    total = numero1 * numero2
+    return total
+
+multiplicar(2,10)
+resultado = multiplicar(5,10)
+print(resultado)
+
+#con print la informacion muere  en cambio lo otro lo puedo guardar en otras operaciones
