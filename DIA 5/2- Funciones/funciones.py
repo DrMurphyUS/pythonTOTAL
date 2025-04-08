4bcb80e8ded3718f0b8644ef0947b923d84b0720
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-08 12:29:13 -0500

    Funciones

---
+++ DIA 5/2- Funciones/funciones.py
@@ -0,0 +1,13 @@
+def saludar_persona():
+    '''
+    Esta función sirve para saludar a las personas 
+    si quiero comentarios de una sola linea uso #
+    '''
+    print('Hola')
+
+saludar_persona()
+
+def saludar_persona2(nombre):
+    print('Hola ' + nombre)
+
+saludar_persona2('Murphy')
