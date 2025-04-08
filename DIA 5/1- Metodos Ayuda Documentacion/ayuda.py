d1124b8ea381b75cfe846a9519f19294465c341b
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-08 12:06:31 -0500

    Ayuda Documentacion

---
+++ DIA 5/1- Metodos Ayuda Documentacion/ayuda.py
@@ -0,0 +1,8 @@
+dic = {'clave1':100,'clave2':250}
+print(dic)
+dicex=dic.popitem()
+print(dic)
+print(dicex)
+# para ayuda es mejor w3schools o documentacion oficial como el pycharm que esperando me manda a la pagina importante
+# ayuda control en pycharm
+# con la documentacion aprenderas cosas que tal vez tus profesores no te enseñan
