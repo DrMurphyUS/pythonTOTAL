df12770a7aa85d511a0bd46be39a50c1ac0177b5
DrMurphyUS <medicosequeiros@gmail.com>
2025-04-15 17:51:46 -0500

    Limpiar la consola

---
+++ DIA 6/6- Limpiar la consola/limpiarconsola.py
@@ -0,0 +1,7 @@
+from os import system
+nombre = input('Dime tu nombre: ')
+edad = input('Dime tu edad: ')
+system('cls')
+print(f"Tu nombre es {nombre} y tu edad es {edad} años")
+#quiero que la pantalla se limpie
+#run->debug->edit configurations->v Emular la terminal de una consola de salida
